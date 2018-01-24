d6831c73493221243de57290a7b7222955daaf3a
DavidMellul <david.mellul@outlook.fr>
2018-01-24 15:04:29 +0100

    Useless lines removed, new version for Pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='Scrapify',
     packages=['Scrapify'],
-    version='5.2',
+    version='5.3',
     description='A simple and intuitive library that allows dead easy web scraping with 3 lines of code.',
     author='David Mellul',
     author_email='david.mellul@outlook.fr',
